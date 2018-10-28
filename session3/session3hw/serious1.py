261327413a63aeb5021627f2de023dd9bb977e1e
Ha Minh <nguyenmduc2000@gmail.com>
2018-10-28 12:07:15 +0700

    initial commit

---
+++ session3/session3hw/serious1.py
@@ -0,0 +1,10 @@
+# 1. nested list is a list that in another list
+# 2. Yes it can
+print("Hi there, welcome to superuser")
+u = input("Username: ")
+p = input("Password: ")
+while (u != "c4e") or (p !="codethechange"):
+    print("Wrong password or wrong username !")
+    u = input("Username: ")
+    p = input("Password: ")
+print ("welcome")
